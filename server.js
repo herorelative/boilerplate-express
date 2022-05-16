874bc4be40406415941dc579f510bb329f16ffb7
zarnimy0 <zarni@getgo.sg>
2022-05-16 19:01:39 +0800

    serve json response

--- server.js
+++ server.js
@@ -44,6 +44,10 @@ app.get('/',(req,res)=>{
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get('/json',(req,res)=>{
+  res.json({"message": "Hello json"});
+});
+
 var port = process.env.PORT || 3000;
 // bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
 //   bGround.log('Node is listening on port '+ port + '...')
