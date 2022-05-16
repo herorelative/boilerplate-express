a18b627bbae1f5c5d556112bde9d33ac1200293f
zarnimy0 <zarni@getgo.sg>
2022-05-16 18:53:23 +0800

    removed static mount

--- server.js
+++ server.js
@@ -38,18 +38,17 @@ if (!process.env.DISABLE_XORIGIN) {
 //   redirect: false
 // };
 
-app.use('/public', express.static(process.cwd() + '/public'));
+//app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/',(req,res)=>{
   res.sendFile(__dirname + "/views/index.html");
 });
 
-
 var port = process.env.PORT || 3000;
 // bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
 //   bGround.log('Node is listening on port '+ port + '...')
 // });
-const listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(port, function () {
   console.log("Node.js listening on port " + listener.address().port);
 });
 
